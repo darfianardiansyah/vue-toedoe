61db2cb5f849a4627f601d956ac699cde1be4928
darfianardiansyah <darfianardiansyah@gmail.com>
2025-08-21 16:24:48 +0700

    Storing Axios Base URL in environment variables

--- src/http/api.js
+++ src/http/api.js
@@ -3,7 +3,7 @@ axios.defaults.withCredentials = true
 axios.defaults.withXSRFToken = true 
 
 const api = axios.create({
-    baseURL:"http://localhost:8000/api/v1"
+    baseURL: import.meta.env.VITE_BASE_URL,
 })
 
 export default api
